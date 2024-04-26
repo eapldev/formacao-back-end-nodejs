726e6bb38c1187f5994584e6b35f20c1fb8c49ce
eapldev <eduardoaparecido.dev@gmail.com>
2024-04-26 19:09:42 -0300

    Criando uma API Completa

---
+++ nodejs-api-instadev/src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const routes = new Router();
+
+routes.get("/health", (require, response) => {
+  return response.send({ message: "Connected with success!" });
+});
+
+module.exports = routes;
