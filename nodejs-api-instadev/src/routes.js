1235d7e010d71b2baef7595d5d85ec3e0ff88858
eapldev <eduardoaparecido.dev@gmail.com>
2024-04-28 12:21:51 -0300

    Criando Middleware de autenticação

--- nodejs-api-instadev/src/routes.js
+++ nodejs-api-instadev/src/routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const schemaValidator = require("./apps/middlewares/schemaValidator");
 
+const AuthenticationMiddleware = require("./apps/middlewares/authentication");
+
 const AuthenticationController = require("./apps/controllers/AuthenticationController");
 const authSchema = require("./schema/auth.schema.json");
 
@@ -11,10 +13,13 @@ const routes = new Router();
 
 routes.post("/user", schemaValidator(userSchema), UserControler.create);
 
-routes.post("/auth", schemaValidator(authSchema), AuthenticationController.authenticate);
+routes.post("/auth",schemaValidator(authSchema), AuthenticationController.authenticate);
+
 
 routes.get("/health", (require, response) => {
   return response.send({ message: "Connected with success!" });
 });
+
+routes.use(AuthenticationMiddleware);
 
 module.exports = routes;
