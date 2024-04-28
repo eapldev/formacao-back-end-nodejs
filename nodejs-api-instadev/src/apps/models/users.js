5092a17b1e83694b8698a3940054961d68985b30
eapldev <eduardoaparecido.dev@gmail.com>
2024-04-28 10:22:38 -0300

    Criando campo virtual

--- nodejs-api-instadev/src/apps/models/users.js
+++ nodejs-api-instadev/src/apps/models/users.js
@@ -1,25 +1,33 @@
 const Sequelize = require("sequelize");
 const { Model } = require("sequelize");
+const bcryptjs = require("bcryptjs");
 
 class Users extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                name: Sequelize.STRING,
-                user_name: Sequelize.STRING,
-                email: Sequelize.STRING,
-                avatar: Sequelize.STRING,
-                bio: Sequelize.STRING,
-                gender: Sequelize.STRING,
-                password_hash: Sequelize.STRING,
-                password: Sequelize.VIRTUAL,
-            },
-            {
-                sequelize,
-            },
-        );
-        return this;
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        user_name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        avatar: Sequelize.STRING,
+        bio: Sequelize.STRING,
+        gender: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
 }
 
 module.exports = Users;
