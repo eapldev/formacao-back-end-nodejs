bba4660193eef632de23b060342be2596353cf2a
eapldev <eduardoaparecido.dev@gmail.com>
2024-05-01 13:02:03 -0300

    Criando método para atualizar os posts

--- nodejs-api-instadev/src/apps/controllers/PostController.js
+++ nodejs-api-instadev/src/apps/controllers/PostController.js
@@ -46,7 +46,35 @@ class PostController {
       return res.status(400).json({ message: "Failed to delete this post!" });
     }
 
-    return res.status(200).json({message: 'Post deleted!'})
+    return res.status(200).json({ message: "Post deleted!" });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const verifyPost = await Posts.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!verifyPost) {
+      return res.status(404).json({ message: "Post does not exists!" });
+    }
+
+    if (verifyPost.author_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ messsage: "You don't have permission to delete this post!" });
+    }
+
+    const postUpdate = await Posts.update(req.body, { where: { id } });
+
+    if (!postUpdate) {
+      return res.status(400).json({ message: "Failed to update this post!" });
+    }
+
+    return res.status(200).json({ message: "Posts updated" });
   }
 }
 
