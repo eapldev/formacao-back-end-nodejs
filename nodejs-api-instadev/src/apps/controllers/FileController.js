78d0207153ca74f4a446d4f68686afa8f97f310b
eapldev <eduardoaparecido.dev@gmail.com>
2024-05-02 21:15:59 -0300

    Realizando upload de imagens para o backblaze e adicionando uma constante para apagar os arquivos temporários do servidor

--- nodejs-api-instadev/src/apps/controllers/FileController.js
+++ nodejs-api-instadev/src/apps/controllers/FileController.js
@@ -1,8 +1,49 @@
+const fsp = require("fs/promises");
+const B2 = require("backblaze-b2");
+
+const b2 = new B2({
+  applicationKeyId: "460f4e93da88",
+  applicationKey: "002643ee0ebc92a6ac0f45c909b0883428dd23727e",
+});
+
+const unlinkAsync = fsp.unlink;
+
 class FileController {
   async upload(req, res) {
-    const { filename } = req.file;
+    const { filename, path } = req.file;
 
-    return res.status(200).json({ url: `uploads/${filename}` });
+    try {
+      const file = await fsp.readFile(`uploads/${filename}`, (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        return data;
+      });
+
+      await b2.authorize();
+
+      const {
+        data: { uploadUrl, authorizationToken },
+      } = await b2.getUploadUrl({
+        bucketId: "c4e690dfa41e39b38dfa0818",
+      });
+
+      const { data } = await b2.uploadFile({
+        uploadUrl: uploadUrl,
+        uploadAuthToken: authorizationToken,
+        fileName: filename,
+        data: file,
+      });
+
+      await unlinkAsync(path);
+
+      return res.send({
+        url: `https://f002.backblazeb2.com/file/bucket-instadev/${data.fileName}`,
+      });
+    } catch (error) {
+      return res.status(400).send({ message: "Failed to upload" });
+    }
   }
 }
 
